2de30269e00147754a6df99d4a24bdf1ca890c03
Sean Altemose <sean@seanaltemose.com>
2018-07-05 03:00:27 +0000

    automatically backed up by learn

--- app/models/concerns/slug_module.rb
+++ app/models/concerns/slug_module.rb
@@ -13,17 +13,3 @@ module SlugThis
       end
     end
 end
-
-module ClassMethods
-    def find_by_slug(slug)
-      self.all.find do |ob|
-        ob.slug == slug
-      end
-    end
-  end
-
-  module InstanceMethods
-    def slug
-      self.username.gsub(/\W/, '-')
-    end
-  end
